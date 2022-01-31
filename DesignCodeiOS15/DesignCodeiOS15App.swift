7832ea6211482ee27cb3a8ef92b2ce72e98848a6
Jonathan Wills <jonathan_wills@icloud.com>
2022-01-31 15:13:34 +0900

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Johny Wills on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
